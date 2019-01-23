8d027ca7386e2863c830e0c46d140ed917599807
emanmoq <42995029+emanmoq@users.noreply.github.com>
2019-01-23 02:35:46 -0800

    fff

---
+++ asset/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    
+    $('.mallCarusel').owlCarousel({
+        autoplay: true,
+        rtl:true,
+        loop: true,
+        dots:true,
+        nav:true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items:1,
+      
+            }
+          
+        },
+
+        
+});
+$('.slider-Owl').owlCarousel({
+    autoplay: true,
+    rtl:true,
+    loop: true,
+    dots:true,
+    nav:true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items:1,
+  
+        }
+      
+    }
+    
+});
+
+});
+
